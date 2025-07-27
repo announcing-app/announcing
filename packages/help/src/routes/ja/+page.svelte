3870c1fb73c0230f4d3ee1da9a4875e1075725ef
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-07-27 19:29:04 +0900

    Add inquiry link

--- packages/help/src/routes/ja/+page.svelte
+++ packages/help/src/routes/ja/+page.svelte
@@ -3,6 +3,7 @@
   <a href="/ja/writer" class="card">お知らせを書く</a>
   <a href="/ja/reader" class="card">お知らせを読む</a>
   <a href="/ja/ios_notification" class="card">iPhone / iPadの通知設定</a>
+  <a href="/ja/inquiry" class="card">問い合わせ</a>
   <a href="/ja/terms" class="card">利用規約</a>
   <a href="/ja/privacy" class="card">プライバシーポリシー</a>
 </div>

5ef9b16eddc5adf4715a74b74cdb179f67c63d9b
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-07-28 18:21:31 +0900

    Update ios_notification

--- packages/help/src/routes/ja/ios_notification/+page.svelte
+++ packages/help/src/routes/ja/ios_notification/+page.svelte
@@ -1,3 +1,45 @@
 <h1>iPhone / iPadの通知設定</h1>
 
-(作成中)
+<!-- prettier-ignore -->
+<div class="text info">
+  Announcingはアプリ無しで新着通知を受け取ることができますが、残念ながらiPhone/iPadでは少し準備が必要です。
+  これはAndroidやWindowsではできることが、iPhone/iPadではまだ十分にできないためです。
+  Appleが対応を行い次第、すぐに改善する予定です。
+</div>
+
+<h2>設定方法</h2>
+
+<img src="/assets/ios_notification/toolbar.jpeg" alt="toolbar" />
+
+<div class="text">チャンネルのページを開いた状態で、画面下メニューの中央のアイコンを押します。</div>
+
+<hr />
+
+<img src="/assets/ios_notification/add_home_ja.jpeg" alt="add home" />
+
+<div class="text">
+  表示されるメニューから「ホーム画面に追加」を押し、表示される画面の右上の「追加」ボタンを押します。
+</div>
+
+<hr />
+
+<img src="/assets/ios_notification/home_icon.jpeg" alt="add home" width="100px" />
+
+<div class="text">ホーム画面に追加された上記のアイコンを押して、設定画面を起動します。</div>
+
+<hr />
+
+<!-- prettier-ignore -->
+<div class="text">
+  チャンネルのIDを入力し、検索ボタンを押すとチャンネルが表示されます。
+  IDはチャンネルページの「IDをコピー」ボタンからコピーできます。
+  通知を受け取りたいチャンネルにチェックを入れて「通知を登録」ボタンを押せば登録完了です。
+</div>
+
+<hr />
+
+<!-- prettier-ignore -->
+<div class="text info">
+  現在は受け取った通知をタップすると、Safariでチャンネルのページが開きます。
+  今後、他のブラウザでも開ける設定を追加する予定です。
+</div>

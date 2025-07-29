cc1337ba60b85454e570ec895a14e05d49a16be6
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-07-29 15:04:34 +0900

    Remove white-space css

--- packages/help/src/routes/ja/ios_notification/+page.svelte
+++ packages/help/src/routes/ja/ios_notification/+page.svelte
@@ -1,9 +1,9 @@
 <h1>iPhone / iPadの通知設定</h1>
 
-<!-- prettier-ignore -->
 <div class="text info">
-  Announcingはアプリ無しで新着通知を受け取ることができますが、残念ながらiPhone/iPadでは少し準備が必要です。
-  これはAndroidやWindowsではできることが、iPhone/iPadではまだ十分にできないためです。
+  Announcingはアプリ無しで新着通知を受け取ることができますが、残念ながらiPhone/iPadでは少し準備が必要です。<br
+  />
+  これはAndroidやWindowsではできることが、iPhone/iPadではまだ十分にできないためです。<br />
   Appleが対応を行い次第、すぐに改善する予定です。
 </div>
 
@@ -29,17 +29,15 @@
 
 <hr />
 
-<!-- prettier-ignore -->
 <div class="text">
-  チャンネルのIDを入力し、検索ボタンを押すとチャンネルが表示されます。
-  IDはチャンネルページの「IDをコピー」ボタンからコピーできます。
+  チャンネルのIDを入力し、検索ボタンを押すとチャンネルが表示されます。<br />
+  IDはチャンネルページの「IDをコピー」ボタンからコピーできます。<br />
   通知を受け取りたいチャンネルにチェックを入れて「通知を登録」ボタンを押せば登録完了です。
 </div>
 
 <hr />
 
-<!-- prettier-ignore -->
 <div class="text info">
-  現在は受け取った通知をタップすると、Safariでチャンネルのページが開きます。
+  現在は受け取った通知をタップすると、Safariでチャンネルのページが開きます。<br />
   今後、他のブラウザでも開ける設定を追加する予定です。
 </div>

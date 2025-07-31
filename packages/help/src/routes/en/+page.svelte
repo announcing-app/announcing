f934dd0d6758a937f55bee2cd4facd3ac1d9527d
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-07-31 11:21:35 +0900

    Fix links

--- packages/help/src/routes/en/+page.svelte
+++ packages/help/src/routes/en/+page.svelte
@@ -1,9 +1,9 @@
 <div class="top-menu">
-  <a href="/ja/concept" class="card">Overview</a>
-  <a href="/ja/writer" class="card">Post an Announcement</a>
-  <a href="/ja/notification" class="card">Notification Settings</a>
-  <a href="/ja/ios_notification" class="card">iPhone/iPad Notification Settings</a>
-  <a href="/ja/inquiry" class="card">Contact</a>
-  <a href="/ja/terms" class="card">Terms of Use</a>
-  <a href="/ja/privacy" class="card">Privacy Policy</a>
+  <a href="/en/concept" class="card">Overview</a>
+  <a href="/en/writer" class="card">Post an Announcement</a>
+  <a href="/en/notification" class="card">Notification Settings</a>
+  <a href="/en/ios_notification" class="card">iPhone/iPad Notification Settings</a>
+  <a href="/en/inquiry" class="card">Contact</a>
+  <a href="/en/terms" class="card">Terms of Use</a>
+  <a href="/en/privacy" class="card">Privacy Policy</a>
 </div>

9e63ec34c348aaacc52a2c9ba9c440d52a66776f
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-07-31 17:37:47 +0900

    Update en inquiry

--- packages/help/src/routes/en/inquiry/+page.svelte
+++ packages/help/src/routes/en/inquiry/+page.svelte
@@ -1,6 +1,6 @@
-<h1>問い合わせ</h1>
+<h1>Contact</h1>
 
 <div class="text">
-  準備中のため<a class="link" href="https://github.com/announcing-app/announcing">Github</a
-  >のIssuesをご利用ください。
+  Please use <a class="link" href="https://github.com/announcing-app/announcing">Github</a> Issues as
+  we are currently preparing for this feature.
 </div>
